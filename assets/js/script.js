35d0624fda29fb12cb4c3cdaaa3337f6ebc5d16f
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-08 02:40:38 -0500

    fixed js and made it work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 var cities = []
 
-var cityFormE1 = document.querySelector("#city-search-form");
-var cityInputE1 = document.querySelector("#city");
-var weatherContainerE1 = document.querySelector("#current-weather-container");
-var citySearchInputE1 = document.querySelector("#searched-city");
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
-var forecastContainerE1 = document.querySelector("#week-container");
-var previousSearchButtonE1 = document.querySelector("#previous-search-btn");
+var forecastContainerEl = document.querySelector("#week-container");
+var previousSearchButtonEl = document.querySelector("#previous-search-btn");
 
-var formSubmitHandler = function(event){
+var formSumbitHandler = function(event){
     event.preventDefault();
-    var city = cityInputE1.ariaValueMax.trim();
+    var city = cityInputEl.value.trim();
     if(city){
         getCityWeather(city);
         getWeek(city);
         cities.unshift({city});
-        cityInputE1.value = "";
+        cityInputEl.value = "";
     } else{
-        alert("Please enter a city");
+        alert("Please enter a City");
     }
     saveSearch();
-    pastSearch(city);
+    previousSearch(city);
 }
 
 var saveSearch = function(){
@@ -39,7 +39,97 @@ var getCityWeather = function(city){
     });
 };
 
- // previousSearch();
+var displayWeather = function(weather, searchCity){
+
+   weatherContainerEl.textContent= "";  
+   citySearchInputEl.textContent=searchCity;
+
+   var currentDate = document.createElement("span")
+   currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+   citySearchInputEl.appendChild(currentDate);
+
+   var weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   citySearchInputEl.appendChild(weatherIcon);
+}
+
+var getWeek = function(city){
+    var apiKey = "1efbe90938445ec45a8146fce795c02c"
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+           displayWeek(data);
+        });
+    });
+};
+
+var displayWeek = function(weather){
+    forecastContainerEl.textContent = ""
+    forecastTitle.textContent = "5-Day Forecast:";
+
+    var forecast = weather.list;
+        for(var i=5; i < forecast.length; i=i+8){
+       var dailyForecast = forecast[i];
+        
+       
+       var forecastEl=document.createElement("div");
+       forecastEl.classList = "card bg-primary text-light m-2";
+
+       //create date element
+       var forecastDate = document.createElement("h5")
+       forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+       forecastDate.classList = "card-header text-center"
+       forecastEl.appendChild(forecastDate);
+
+       
+       //create an image element
+       var weatherIcon = document.createElement("img")
+       weatherIcon.classList = "card-body text-center";
+       weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
+
+       //append to forecast card
+       forecastEl.appendChild(weatherIcon);
+       
+       //create temperature span
+       var forecastTempEl=document.createElement("span");
+       forecastTempEl.classList = "card-body text-center";
+       forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+
+        //append to forecast card
+        forecastEl.appendChild(forecastTempEl);
+
+       var forecastHumEl=document.createElement("span");
+       forecastHumEl.classList = "card-body text-center";
+       forecastHumEl.textContent = dailyForecast.main.humidity + "  %";
+
+       //append to forecast card
+       forecastEl.appendChild(forecastHumEl);
+
+       //append to week container
+        forecastContainerEl.appendChild(forecastEl);
+    }
+}
+
+var previousSearch = function(previousSearch){
  
- cityFormEl.addEventListener("submit", formSumbitHandler);
- previousSearchButtonEl.addEventListener("click", previousSearchHandler);
+    previousSearchEl = document.createElement("button");
+    previousSearchEl.textContent = previousSearch;
+    previousSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    previousSearchEl.setAttribute("data-city",previousSearch)
+    previousSearchEl.setAttribute("type", "submit");
+
+    previousSearchButtonEl.prepend(previousSearchEl);
+}
+
+var previousSearchHandler = function(event){
+    var city = event.target.getAttribute("data-city")
+    if(city){
+        getCityWeather(city);
+        getWeek(city);
+    }
+}
+
+cityFormEl.addEventListener("submit", formSumbitHandler);
+previousSearchButtonEl.addEventListener("click", previousSearchHandler);
